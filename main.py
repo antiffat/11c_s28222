7da930cdc1ec316841c737d5fe16c353181a4401
s28222 <s28222@pjwstk.edu.pl>
2024-03-18 11:20:47 +0100

    Task 2: Implemented function generate_squares to generate squares in a given range as parameters

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Task 1:
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+# Task 2:
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end + 1)]
+
+squares = generate_squares(1, 10)
+print("List of squares from 1 to 10: ", squares)
+

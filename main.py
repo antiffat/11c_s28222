2a6f15c8a3d187c0812079f5e66b0a4f4d2bb578
s28222 <s28222@pjwstk.edu.pl>
2024-03-18 11:35:08 +0100

    Task 5: Implementation of error handling if the end range is smaller than the start range for the list and usage of it:

--- main.py
+++ main.py
@@ -24,9 +24,13 @@ class SquareGenerator:
         return [math.sqrt(x) for x in squares]
 
 generator = SquareGenerator()
-squares = generator.generate_squares(2, 8)
-square_roots = generator.calculate_square_roots(squares)
+try:
+    squares = generator.generate_squares(1, 10)
+    print("List of squares from 1 to 10:", squares)
 
-print("List of squares from 2 to 8:", squares)
-print("Square roots of the generated squares: ", square_roots)
+    square_roots = generator.calculate_square_roots(squares)
+    print("Square roots of the generated squares:", square_roots)
+
+except ValueError as e:
+    print(f"Error: {e}")
 

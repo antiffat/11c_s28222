a50d672fb3ce7b7a14575cfa11b6364c69d12343
s28222 <s28222@pjwstk.edu.pl>
2024-03-18 11:25:50 +0100

    Task 3: Implemented SquareGenerator class and usage of it

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def generate_squares(start, end):
 squares = generate_squares(1, 10)
 print("List of squares from 1 to 10: ", squares)
 
+# Task 3:
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x ** 2 for x in range(start, end + 1)]
+
+generator = SquareGenerator()
+squares = generator.generate_squares(2, 8)
+
+print("List of squares from 2 to 8:", squares)

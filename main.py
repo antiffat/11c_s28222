f16ebee926de13b55923cbbb11f1f10467d889da
s28222 <s28222@pjwstk.edu.pl>
2024-03-18 11:30:22 +0100

    Task 4: added function calculate_square_roots() to the SquareGenerator class and usage of it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 # Task 1:
 squares = [x**2 for x in range(1, 11)]
 print(squares)
@@ -14,7 +16,14 @@ class SquareGenerator:
     def generate_squares(self, start, end):
         return [x ** 2 for x in range(start, end + 1)]
 
+    # Task 4:
+    def calculate_square_roots(self, squares):
+        return [math.sqrt(x) for x in squares]
+
 generator = SquareGenerator()
 squares = generator.generate_squares(2, 8)
+square_roots = generator.calculate_square_roots(squares)
 
 print("List of squares from 2 to 8:", squares)
+print("Square roots of the generated squares: ", square_roots)
+

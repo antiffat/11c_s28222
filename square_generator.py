3af4d11d7eb3ea4aa8bf0ec13145bc3edf615154
s28222 <s28222@pjwstk.edu.pl>
2024-03-24 08:43:09 +0100

    Solution to task 6: class square_generator moved from main.py to new module, necessary imports made to main.py

---
+++ square_generator.py
@@ -0,0 +1,12 @@
+import math
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        # Task 5 (Exceptions):
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+        return [x ** 2 for x in range(start, end + 1)]
+
+    # Task 4 (Libraries):
+    def calculate_square_roots(self, squares):
+        return [math.sqrt(x) for x in squares]
